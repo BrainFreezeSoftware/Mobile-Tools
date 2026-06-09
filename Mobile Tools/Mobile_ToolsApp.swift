14fb5487da02be3e709730dc1e2a49a5beb96503
BrainFreezeSoftware <steve@brainfreezesoftware.com>
2026-06-09 15:05:47 -0400

    Initial Commit

---
+++ Mobile Tools/Mobile_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_ToolsApp.swift
+//  Mobile Tools
+//
+//  Created by Steve Stasinos on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
